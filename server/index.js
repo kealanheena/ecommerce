5b577a006f7292f952b8d68587261329b2637c45
Conor Heena <conorheena@Conors-MacBook-Pro-5.local>
2020-05-15 19:22:21 +0100

    setup database connection and modified gitignore

--- server/index.js
+++ server/index.js
@@ -2,11 +2,23 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const PORT = 4000;
 
 app.use(cors());
 app.use(bodyParser.json);
 
+mongoose.connect('mongodb://127.0.0.1.27017/ecommerce', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+  console.log('connected to mongoDB');
+});
+
 app.listen(PORT, () => {
-    console.log(`App is listening on port ${PORT}`)
+  console.log(`App is listening on port ${PORT}`);
 });
