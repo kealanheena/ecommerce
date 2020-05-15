1ffed42ebf989d8f23d7c1d9857497380626e26e
Conor Heena <conorheena@Conors-MacBook-Pro-5.local>
2020-05-15 17:25:55 +0100

    added react router for different pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+import Home from './pages/Home';
+import PDP from './pages/PDP';
+import PLP from './pages/PLP';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Ecom App</h1>
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">home</Link>
+            </li>
+            <li>
+              <Link to="/products">plp</Link>
+            </li>
+            <li>
+              <Link to="/products/:id">pdp</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route exact path="/" component={Home} />
+        <Route exact path="/products" component={PLP} />
+        <Route exact path="/products/:id" component={PDP} />
+      </Router>
     </div>
   );
 }
---
+++ src/pages/PLP.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function PLP() {
+  return (
+    <div className="plpPage">
+      <h2>PLP page</h2>
+    </div>
+  );
+}
+
+export default PLP;
