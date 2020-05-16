fd009be892b6ebf68958bf052c7ffb62b7d545ae
Conor Heena <conorheena@Conors-MacBook-Pro-5.local>
2020-05-16 14:23:02 +0100

    added shopping cart icon

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import { Navbar, Nav } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 function Navigation() {
   return (
@@ -10,14 +12,13 @@ function Navigation() {
         Ecommerce
       </Navbar.Brand>
       <Nav className="mr-auto">
-        <Nav.Link as={Link} to="/">
-          Home
-        </Nav.Link>
         <Nav.Link as={Link} to="/products">
           products
         </Nav.Link>
-        <Nav.Link as={Link} to="/products/:id">
-          pdp
+      </Nav>
+      <Nav className="ml-auto">
+        <Nav.Link>
+          <FontAwesomeIcon color="#fff" icon={faShoppingCart} />
         </Nav.Link>
       </Nav>
     </Navbar>
