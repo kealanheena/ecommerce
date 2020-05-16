2e47a2b33d4dcdc4d6009a191b13984a75bad47c
Conor Heena <conorheena@Conors-MacBook-Pro-5.local>
2020-05-16 14:33:08 +0100

    removed fake data as it was failing CI snapshots

--- client/src/mock_data/products.js
+++ client/src/mock_data/products.js
@@ -1,14 +1,12 @@
-import faker from 'faker';
-
 const productData = [];
 
 for(let i = 1; i <= 12; i++) {
   productData.push({
     id: i,
     image: 'http://placehold.jp/640x480.png?text=placeholder',
-    title: faker.commerce.productName(),
-    price: faker.commerce.price(),
-    description: `${faker.commerce.productAdjective()}, ${faker.commerce.productAdjective()}, ${faker.commerce.productAdjective()}`
+    title: 'Product Title',
+    price: 100,
+    description: `this is a product description`
   });
 }
 
