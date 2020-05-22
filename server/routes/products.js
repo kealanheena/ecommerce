41b87327308d302fd5a5439c02574c5d992854f1
kealanheena <kealanheena@gmail.com>
2020-05-23 00:28:12 +0100

    Testing CI

--- server/routes/products.js
+++ server/routes/products.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Product = require('../models/product')
 
+// Test
 // Get all products
 router.get('/', async (req, res) => {
   try {
