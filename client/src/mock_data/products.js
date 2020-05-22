767efef4aff5eb82e036e3371f2f25c093984283
Conor Heena <conorheena@Conors-MacBook-Pro-5.local>
2020-05-22 22:54:42 +0100

    testing CI

--- client/src/mock_data/products.js
+++ client/src/mock_data/products.js
@@ -1,6 +1,6 @@
 const productData = [];
 
-for(let i = 1; i <= 12; i + 1) {
+for(let i = 1; i <= 12; i = i+1) {
   productData.push({
     id: i,
     image: 'http://placehold.jp/640x480.png?text=placeholder',
